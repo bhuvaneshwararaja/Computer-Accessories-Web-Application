1d53e6dfabadebc9e07a222789126448b6907fcb
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-03 02:21:32 +0530

    adding new test Route

--- ecom-client/src/App.js
+++ ecom-client/src/App.js
@@ -2,11 +2,12 @@
 import './index.css';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Product from "./Pages/Admin/product"
+import ProductView from "./Pages/Admin/productView"
 function App() {
   return <>
   <Router>
     <Switch>
-      <Route path="/admin/product" exact component={Product}></Route>
+      <Route path="/admin/product" exact component={ProductView}></Route>
     </Switch>
   </Router>
   </>
