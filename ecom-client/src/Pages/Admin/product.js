470748d15ebca0d4bc8bcfa9e6baf99926c899f8
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-03 20:01:33 +0530

    fixing bug

--- ecom-client/src/Pages/Admin/product.js
+++ ecom-client/src/Pages/Admin/product.js
@@ -83,7 +83,8 @@ const Product = () => {
                 </div>
                 <div className="text-center">
                <button className="border-transparent rounded-xl transition-all duration-500 text-2xl px-3 bg-green-600 text-white hover:bg-green-700 m-3 " onClick={(e) => {
-                   fetch("/admin/add",{
+                   e.preventDefault()
+                   fetch("/admin/add/",{
                     'method':"POST",
                        headers:{
                             
