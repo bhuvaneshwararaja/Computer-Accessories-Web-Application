7c95f437fdea4f8da10925b62e18692152894934
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-08 01:28:59 +0530

    initializing User Page

---
+++ ecom-client/src/Pages/User/Home.js
@@ -0,0 +1,12 @@
+import UserNav from "../../Components/UserNav"
+
+const Home = () => {
+    return <>
+    <div className="w-11/12 h-auto m-auto">
+    <UserNav />
+    </div>
+     
+    </>
+}
+
+export default Home;
