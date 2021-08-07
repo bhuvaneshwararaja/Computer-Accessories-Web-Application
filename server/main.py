4d07c456e49925d6e7075104258a64ca7765b397
Mathesh-kumar <matheshkumar256@gmail.com>
2021-08-07 20:46:32 +0530

    basic user functions(title) added

--- server/main.py
+++ server/main.py
@@ -88,10 +88,41 @@ class User(Resource):
     def __init__(self):
         pass
 
+    @staticmethod
+    def home_page():
+        pass
 
-app.add_url_rule('/admin/add/', view_func=Admin.add_product, methods=['POST'])
-app.add_url_rule('/admin/view/', view_func=Admin.view_product, methods=['GET'])
-app.add_url_rule('/admin/remove/', view_func=Admin.remove_product, methods=['POST'])
+    @staticmethod
+    def sign_up():
+        pass
+
+    @staticmethod
+    def sign_in():
+        pass
+
+    @staticmethod
+    def order_products():
+        pass
+
+    @staticmethod
+    def order_cancel():
+        pass
+
+    @staticmethod
+    def order_track():
+        pass
+
+
+app.add_url_rule('/admin/add/', view_func=Admin.add_product(), methods=['POST'])
+app.add_url_rule('/admin/view/', view_func=Admin.view_product(), methods=['GET'])
+app.add_url_rule('/admin/remove/', view_func=Admin.remove_product(), methods=['POST'])
+
+app.add_url_rule('/user/home/', view_func=User.home_page(), methods=['GET'])
+app.add_url_rule('/user/signup/', view_func=User.sign_up(), methods=['GET'])
+app.add_url_rule('/user/signin/', view_func=User.sign_in(), methods=['GET'])
+app.add_url_rule('/user/order/', view_func=User.order_products(), methods=['GET'])
+app.add_url_rule('/user/cancel/', view_func=User.order_cancel(), methods=['GET'])
+app.add_url_rule('/user/track/', view_func=User.order_track(), methods=['GET'])
 
 
 if __name__ == '__main__':
