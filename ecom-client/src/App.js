081a235be3d456e83128e9769ea738f26a487b51
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-10 14:05:33 +0530

    adding route

--- ecom-client/src/App.js
+++ ecom-client/src/App.js
@@ -3,15 +3,13 @@ import './index.css';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import ProductView from "./Pages/Admin/productView"
 import __404 from "./Error/404"
-import CkartNavigation from "./Components/ckartNavigation/"
 import CkartHome from "./Pages/ckart/CkartHome"
 function App() {
   return <>
   <Router>
-    <CkartNavigation />
     <Switch>
-      {/* <Route path="/" component={CkartHome} exact /> */}
-      {/* <Route component={__404}/> */}
+      <Route path="/" component={CkartHome} exact /> 
+      <Route component={__404}/>
     </Switch>
   </Router>
   </>
