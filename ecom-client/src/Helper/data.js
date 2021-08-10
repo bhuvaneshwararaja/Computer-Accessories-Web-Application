3a79c4a268ffccab53d249296983435421e36d0f
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-10 20:13:41 +0530

    fetching products

--- ecom-client/src/Helper/data.js
+++ ecom-client/src/Helper/data.js
@@ -2,13 +2,8 @@
 let product = {}
 let productId = []
 
-fetch("/admin/view/")
-.then(res => res.json())
-.then((data) => {
-        product = data.products
-        productId = Object.keys(data.products)
-})
 
+console.log(product)
 
 
 export {product,productId}
