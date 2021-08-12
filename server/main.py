b6d42fc3b423216c562729454351992326ff17d5
Mathesh-kumar <matheshkumar256@gmail.com>
2021-08-12 19:18:04 +0530

    user, user_mongo py file created

--- server/main.py
+++ server/main.py
@@ -1,53 +1,25 @@
 # Import needed libraries
-import flask
-from flask import Flask, jsonify
-from flask_restful import Api, Resource
-import pymongo
-from bson.objectid import ObjectId
+from flask import Flask
+from flask_restful import Api
 from admin import Admin
+from user import User
 
+# Flask app creation
 app = Flask(__name__)
 api = Api(app)
 
-class UserMongo(Resource):
-    def __init__(self):
-        pass
-
-class User(Resource):
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def sign_up():
-        pass
-
-    @staticmethod
-    def sign_in():
-        pass
-
-    @staticmethod
-    def order_products():
-        pass
-
-    @staticmethod
-    def order_cancel():
-        pass
-
-    @staticmethod
-    def order_track():
-        pass
-
-
+# Admin route functions
 app.add_url_rule('/admin/add/', view_func=Admin.add_product, methods=['POST'])
 app.add_url_rule('/admin/view/', view_func=Admin.view_product, methods=['GET'])
 app.add_url_rule('/admin/remove/', view_func=Admin.remove_product, methods=['POST'])
 
+# User route functions
 app.add_url_rule('/user/signup/', view_func=User.sign_up, methods=['GET'])
 app.add_url_rule('/user/signin/', view_func=User.sign_in, methods=['GET'])
 app.add_url_rule('/user/order/', view_func=User.order_products, methods=['GET'])
 app.add_url_rule('/user/cancel/', view_func=User.order_cancel, methods=['GET'])
 app.add_url_rule('/user/track/', view_func=User.order_track, methods=['GET'])
 
-
+# Python main function
 if __name__ == '__main__':
     app.run(debug=True)
