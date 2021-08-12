a885fd2e2773fee4d3a01661ccf296a997f5f687
Mathesh-kumar <matheshkumar256@gmail.com>
2021-08-12 18:51:20 +0530

    admin, admin_mongo py file created

--- server/main.py
+++ server/main.py
@@ -4,81 +4,10 @@ from flask import Flask, jsonify
 from flask_restful import Api, Resource
 import pymongo
 from bson.objectid import ObjectId
+from admin import Admin
 
 app = Flask(__name__)
 api = Api(app)
-
-class AdminMongo:
-    def __init__(self):
-        pass
-    
-    @staticmethod
-    def credential():
-        client = pymongo.MongoClient(f"mongodb+srv://admin:admin@admin.7iagg.mongodb.net/ComputerAccessories?ssl=true&ssl_cert_reqs=CERT_NONE")
-        dataBase = client["ComputerAccessories"]
-        return dataBase
-    
-    @staticmethod
-    def create_record(product_details):
-        dataBase = AdminMongo.credential()
-        collection = dataBase["Products"]
-        collection.insert_one(product_details)
-
-    @staticmethod
-    def view_records():
-        products = {'products': {}}
-        dataBase = AdminMongo.credential()
-        for product in enumerate(dataBase["Products"].find({})):
-            products["products"][str(product[1].pop('_id'))] = product[1]
-        return products
-
-    @staticmethod
-    def remove_records(product_ids):
-        dataBase = AdminMongo.credential()
-        collection = dataBase["Products"]
-        for ids in product_ids:
-            collection.delete_one({"_id": ObjectId(ids)})
-
-class Admin(Resource):
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def add_product():
-        try:
-            product = flask.request.json
-        except:
-            return jsonify({"ReplyCode": "0", "ReplyMessage": "Error in json object receive during add product"})
-
-        try:
-            AdminMongo.create_record(product['test'])
-        except:
-            return jsonify({"ReplyCode": "0", "ReplyMessage": "Error in mongo data insertion"})
-
-        return jsonify({"ReplyCode": "1", "ReplyMessage": "Success"})
-
-    @staticmethod
-    def view_product():
-        try:
-            product = AdminMongo.view_records()
-        except:
-            return jsonify({"ReplyCode": "0", "ReplyMessage": "Error in mongo data retrieval"})
-
-        return jsonify(product)
-
-    @staticmethod
-    def remove_product():
-        try:
-            product = flask.request.json
-        except:
-            return jsonify({"ReplyCode": "0", "ReplyMessage": "Error in json object receive during remove product"})
-
-        try:
-            AdminMongo.remove_records(product['test']['productId'])
-        except:
-            return jsonify({"ReplyCode": "0", "ReplyMessage": "Error in mongo data deletion"})
-
-        return jsonify({"ReplyCode": "1", "ReplyMessage": "Success"})
 
 class UserMongo(Resource):
     def __init__(self):
