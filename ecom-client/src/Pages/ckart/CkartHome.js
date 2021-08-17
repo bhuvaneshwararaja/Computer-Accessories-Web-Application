9fba59e16233e936072694faf65b3220bf5911ca
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-18 01:50:51 +0530

    updating home UI

--- ecom-client/src/Pages/ckart/CkartHome.js
+++ ecom-client/src/Pages/ckart/CkartHome.js
@@ -5,6 +5,7 @@ import {getProducts as listProducts} from "../../Redux/actions/productActions"
 import {IoMdArrowDropright, IoMdArrowDropleft} from "react-icons/io"
 import BannerSlider from "./slider"
 import FeaturedCard from "../../Components/featured/featuredCard"
+import CarouselCards from "../../Components/Carousel"
 const CkartHome = () => {
     
     const dispatch = useDispatch()
@@ -21,13 +22,17 @@ const CkartHome = () => {
 
     <CkartNavigation />   
         <section className="w-11/12 h-screen  relative top-52 m-auto">
-        {loading === false ? (
-
+        {loading === false ? (  
+            <>
             <BannerSlider product={products.products}/>
+            <h1 className="font-heading text-center mt-3 text-4xl">Featured Catagory</h1>
+            <FeaturedCard image={["Adaptors","Audio&VideoAccessories","Components","Keyboard&Mouse","LaptopAccessories","StorageDevices"]} />
+            <CarouselCards data={products.products} keyVal={Object.keys(products.products)}/>
+            </>
         ) :""}
-        <h1 className="font-heading text-center mt-3 text-4xl">Featured Catagory</h1>
-        <FeaturedCard image={["Adaptors","Audio&VideoAccessories","Components","Keyboard&Mouse","LaptopAccessories","StorageDevices"]} />
+
         </section>
+        
     </>
 }
 export default CkartHome;
