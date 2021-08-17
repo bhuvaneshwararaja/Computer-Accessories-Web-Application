7977905b1f5f201e13ec1a19223128a21dc8fddc
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-18 01:50:29 +0530

    getting data

--- ecom-client/src/Components/Carousel/index.js
+++ ecom-client/src/Components/Carousel/index.js
@@ -1,10 +1,15 @@
 import {useState,useEffect} from "react"
 import {useSelector,useDispatch} from "react-redux"
-import {getProducts as listProducts} from "../Redux/actions/productActions"
+import {getProducts as listProducts} from "../../Redux/actions/productActions"
 
-const CarouselCards  = ({category}) => {
-    const dispatch = useDispatch()
-    const getProductList = useSelector((state) => state.getProduct)
+const CarouselCards  = ({category,data,keyVal}) => {
+   
+    const specifiCategory = keyVal.filter((val,index) => {
+        return data[val].productCatagory === "Audio & Video accessories"
+    })
+    
+
+    console.log(specifiCategory)
     return <>
         hi
     </>
