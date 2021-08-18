b941b31778c4f0f4e5e79066abefcb27904a5b4d
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-19 03:07:56 +0530

    scrolling events

--- ecom-client/src/Components/ckartNavigation/index.js
+++ ecom-client/src/Components/ckartNavigation/index.js
@@ -29,10 +29,33 @@ const CkartNavigation = () => {
             setToogleSearch("open")
         }
     }
-
+    window.addEventListener("scroll", () => {
+        let navBar = document.querySelector("nav")
+        let searchBar = document.querySelector(".search-bar")
+        if(window.scrollY >= 150){
+            navBar.classList.remove("w-11/12")
+            navBar.classList.add("w-full")
+            navBar.classList.remove("left-20")
+            navBar.classList.remove("top-10")
+            searchBar.classList.remove("right-16")
+            searchBar.classList.add("right-0")
+            searchBar.classList.remove("top-32")
+            searchBar.classList.add("top-24")
+        }
+        else{
+            navBar.classList.add("w-11/12")
+            navBar.classList.remove("w-full")
+            navBar.classList.add("left-20")
+            navBar.classList.add("top-10")
+            searchBar.classList.add("right-16")
+            searchBar.classList.remove("right-0")
+            searchBar.classList.add("top-32")
+            searchBar.classList.remove("top-24")
+        }
+    })
     return <>
         <header className="w-full flex flex-col ">
-            <nav className="flex justify-between items-center h-16 left-20 top-10 text-xl px-4 font-heading shadow-md fixed w-11/12  m-auto rounded-3xl z-50 bg-white">
+            <nav className="flex justify-between items-center h-16 left-20 top-10 text-xl px-4 font-heading shadow-md fixed w-11/12  m-auto rounded-3xl z-50 bg-white transition-all duration-500">
                 <Link to="" className="font-logo text-2xl">C-Kart</Link>
 
                 
@@ -61,7 +84,7 @@ const CkartNavigation = () => {
                     setModal(true)
                 }}>signin/register</Link>
             </nav>
-            <div className="fixed right-16 w-5 expand transition-all duration-500 flex flex-row-reverse top-32 z-50">
+            <div className="fixed right-16 w-5 expand transition-all duration-500 flex flex-row-reverse top-32 z-50 search-bar">
                 <input type="text" className="border-2 invisible opacity-0 w-0 search transition-all duration-500 px-2 focus:border-indigo-500" placeholder="Search products here !..."></input>
                 <button className="absolute px-4 py-1.5 bg-indigo-500 text-white text-xl btn-search invisible opacity-0 transition-all duration-500">search</button>
                 <button className="border-2 px-4 text-3xl rounded-3xl py-1 bg-indigo-600 text-white"onClick={ToogleBtnSearch}>{toogleSearch === "open" ? <IoMdSearch /> : <IoMdCloseCircle />}</button>
