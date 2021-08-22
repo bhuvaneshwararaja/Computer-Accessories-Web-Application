d59df6959e04e37a63838d47718236a2ae5f9f8f
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-08-22 20:02:05 +0530

    catagory UI

---
+++ ecom-client/src/Pages/ckart/Catagory.js
@@ -0,0 +1,32 @@
+import {useState,useEffect} from "react"
+import {useSelector,useDispatch} from "react-redux"
+import CkartNavigation from "../../Components/ckartNavigation"
+import CkartFooter from "../../Components/Footer"
+import ProductCards from "../../Components/ProductCards"
+import {getProducts as listProducts} from "../../Redux/actions/productActions"
+
+
+const Catagory = () => {
+    const dispatch = useDispatch()
+    const getProductList =useSelector((state) => state.getProduct)
+   
+    const {loading,products} = getProductList
+    console.log(products.products)
+    useEffect(() => {
+        dispatch(listProducts())
+    },[dispatch])
+    return <>
+    <CkartNavigation />
+        {loading === false ? (<>
+            <div className="w-11/12  h-full m-auto flex justify-center flex-wrap relative top-36">
+            
+            {Object.keys(products.products).map((val,index) => {
+                return <ProductCards product={products.products[val]} id={val}/>
+            })}
+            </div>
+        </>):""}
+        <CkartFooter/>
+    </>
+}
+
+export default Catagory;
